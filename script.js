904bb210f35b0c43ec54917c549e057edd20a6db
shuvo2712 <shuvo2712@gmail.com>
2025-07-27 04:43:37 +0600

    first javascript

---
+++ script.js
@@ -0,0 +1,30 @@
+// var
+// let
+// const
+// console.log( ? );
+
+
+var aa = isNaN(2+7);
+console.log(aa)
+
+var ab = isNaN(`11`);
+console.log(ab);
+
+var ac = isNaN('22');
+console.log(ac);
+
+var ad = isNaN("33");
+console.log(ad);
+
+var ae = isNaN("abc");
+console.log(ae);
+
+
+var ba = 11;
+var bb = ba / 5;
+console.log(bb)
+
+var bc = 119;
+var bd = bc % 5;
+console.log(bd)
+
